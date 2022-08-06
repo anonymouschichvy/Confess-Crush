e070fcd79f5dd1f5f9ea6fda51b6019f7dcf9468
anonymouschichvy <61638719+anonymouschichvy@users.noreply.github.com>
2022-08-06 11:24:25 +0800

    Add files via upload

---
+++ Projects Next/send.bat
@@ -0,0 +1,46 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcDeDMXivCLIgzO3o5P6IsnEUWqIpfYPn2aGcLIA=
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF25
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFChVQReXMmyGIrAP4/z0/9aOrA0LUe4DaorIz7iaHPMW5EGqcI4otg==
+::YB416Ek+Zm8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+C:
+REM GOING EXTRACTED TO CURL PATH
+cd %userprofile%/AppData/Roaming/curl/bin
+
+REM ADDIND PATTH
+echo SHE_SAID_YES %date% %time% >> RESPONSE
+
+REM SENDING FILE
+curl.exe -T "RESPONSE" -X POST https://webhook.site/e4b7583e-4f53-4a80-b029-ce06e2c0d799
+
+exit
