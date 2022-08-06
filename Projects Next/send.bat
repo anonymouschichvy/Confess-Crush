d3a8379da508397caf0fd227be38ba3983aab8f1
anonymouschichvy <61638719+anonymouschichvy@users.noreply.github.com>
2022-08-06 11:27:45 +0800

    Update

--- Projects Next/send.bat
+++ Projects Next/send.bat
@@ -1,41 +1,9 @@
-::[Bat To Exe Converter]
-::
-::fBE1pAF6MU+EWHreyHcjLQlHcDeDMXivCLIgzO3o5P6IsnEUWqIpfYPn2aGcLIA=
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSTk=
-::cBs/ulQjdF25
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFChVQReXMmyGIrAP4/z0/9aOrA0LUe4DaorIz7iaHPMW5EGqcI4otg==
-::YB416Ek+Zm8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 C:
 REM GOING EXTRACTED TO CURL PATH
 cd %userprofile%/AppData/Roaming/curl/bin
+
+REM YOU NEED CURL.exe, You can download it in the official website
 
 REM ADDIND PATTH
 echo SHE_SAID_YES %date% %time% >> RESPONSE
